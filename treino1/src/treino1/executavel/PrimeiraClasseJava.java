3c0d6f259c4e30bb68099b903e6d0e2142aa627e
snk_f <snk_f@DESKTOP-ENCE1UO>
2022-12-07 18:48:50 -0300

    teste

--- treino1/src/treino1/executavel/PrimeiraClasseJava.java
+++ treino1/src/treino1/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package treino1.executavel;
 
 public class PrimeiraClasseJava {
 	
-	//teste primeiro commit
+	//teste 
 	public static void main(String[] args) {
 		System.out.println("olá mundo");
 	}
